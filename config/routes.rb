2aedfe90e994cdc3c46476c528d2f2356aa44056
David Hevesi <davidhevesi2008@gmail.com>
2019-07-17 12:29:14 -0400

    add root to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :iteams
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'iteams#index'
 end
